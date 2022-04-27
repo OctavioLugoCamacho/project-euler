37a6a710f4d70206013ead078895152ec3a18f72
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-26 21:25:31 -0500

    Add the tenth Exercise

---
+++ problem_10.groovy
@@ -0,0 +1,22 @@
+long sum = 0
+for(long i in 2..2000000) {
+	if(isPrime(i)) {
+		sum += i
+	}
+}
+println sum
+
+def boolean isPrime(long n) {
+  def count = 0
+  long sqrt = (long)Math.sqrt(n)
+
+  for(long i in 1..sqrt) {
+    if(n % i == 0) {
+      count++
+    }
+    if(count > 1) {
+      return false
+    }
+  }
+  return true
+}
