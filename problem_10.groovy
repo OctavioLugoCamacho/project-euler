3102e29b3c6da5a7e646735a83c0320dde8f6600
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-07-20 16:57:33 -0500

    Refactor: ultima optimizacion

--- problem_10.groovy
+++ problem_10.groovy
@@ -3,22 +3,11 @@ Long sum = (2..2000000).collect { Long n ->
 }.sum()
 println sum
 
-def boolean isPrime(long n) {
+def boolean isPrime(Long n) {
   def count = 0
-  Long sqrt = Math.sqrt(n)
-
-  // count = (2..sqrt).findAll { Long i -> 
-  //   i % 2 == 1
-  // }.collect { Long i ->
-  //   (n % i == 0) ? 1 : 0
-  // }.sum()
-  // !icount > 1)
-
-  for(Long i in 1..sqrt) {
-    if(n % i == 0) count++ 
-    if(count > 1) {
-      return false
-    }
+  for(Long i in 1..Math.sqrt(n)) {
+    if(n % i == 0) count++
+    if(count > 1) return false
   }
-  return true
+  true
 }
