9d45706c0880a93ff891274083459139688a4509
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-07-20 16:53:53 -0500

    Refactor: condicionales

--- problem_10.groovy
+++ problem_10.groovy
@@ -1,22 +1,21 @@
-long sum = 0
-sum = (2..2000000).collect { Long n ->
+Long sum = (2..2000000).collect { Long n ->
   isPrime(n) ? n : 0
 }.sum()
-// for(long i in 2..2000000) {
-// 	if(isPrime(i)) {
-// 		sum += i
-// 	}
-// }
 println sum
 
 def boolean isPrime(long n) {
   def count = 0
-  long sqrt = (long)Math.sqrt(n)
+  Long sqrt = Math.sqrt(n)
 
-  for(long i in 1..sqrt) {
-    if(n % i == 0) {
-      count++
-    }
+  // count = (2..sqrt).findAll { Long i -> 
+  //   i % 2 == 1
+  // }.collect { Long i ->
+  //   (n % i == 0) ? 1 : 0
+  // }.sum()
+  // !icount > 1)
+
+  for(Long i in 1..sqrt) {
+    if(n % i == 0) count++ 
     if(count > 1) {
       return false
     }
